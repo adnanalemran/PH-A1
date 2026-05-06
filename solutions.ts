242bdb7a8ebf5e74e01420274df22fc0d94772e2
Adnan Al-Emran <68654730+adnanalemran@users.noreply.github.com>
2026-05-07 00:04:44 +0600

    a function reverseString that takes a string as input and returns the reversed version of that string.

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,9 @@
 function filterEvenNumbers(arr: number[]): number[] {
   return arr.filter(num => num % 2 === 0);
 }
+
+// Problem 2
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+ 
